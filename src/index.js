ee6de1794b7f621095f90177637d6c25ba5a8471
Mikael <50752113+MikaelVV@users.noreply.github.com>
2024-07-14 15:15:56 +0300

    Creating person object and adding user input values to it

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@
 let number1 = 1;
 let number2 = 2;
 let calculation;
+let firstNameValue;
+let lastNameValue;
 
+// Person object that holds all the information.
+let person = {
 
+    firstName:"" ,
+    lastName: "",
+    age: 0,
+    email: "",
+    address: ""
+
+};
 
 function appendToDisplayScreen(){
  calculation = number1 + number2;
@@ -12,10 +23,18 @@ function appendToDisplayScreen(){
 }
 
 function showResults(){
-    let firstNameBox = document.getElementById("firstname");
-    let lastNameBox = document.getElementById("lastname");
-    let firstNameValue = firstNameBox.value;
-    let lastNameValue = lastNameBox.value;
+    let firstNameBox = document.getElementById("firstname").value;
+    let lastNameBox = document.getElementById("lastname").value;
+    let ageBox = document.getElementById("age").value;
+    let emailBox = document.getElementById("email").value;
+    let adressBox = document.getElementById("adress").value;
+    
+     person.firstName = firstNameBox;
+     person.lastName = lastNameBox;
+     person.age = ageBox;
+     person.email = emailBox;
+     person.address = adressBox;
 
-    console.log(number1 +"\n" + firstNameValue + "\n" + lastNameValue)
+    console.log(number1 +"\n" + person.firstName + "\n" + person.lastName)
+    console.log(person)
 }
