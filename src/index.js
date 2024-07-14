fb52ced083816ff632bd6e5866f3c232658148b2
Mikael <50752113+MikaelVV@users.noreply.github.com>
2024-07-14 15:42:48 +0300

    Tweaking the values a little bit and starting to make tables for listing all the infromation.

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ let firstNameValue;
 let lastNameValue;
 
 // Person object that holds all the information.
-let person = {
+let person = [
 
-    firstName:"" ,
-    lastName: "",
-    age: 0,
-    email: "",
-    address: ""
+    {firstName:""} ,
+    {lastName: ""},
+    {age: 0},
+    {email: ""},
+    {address: ""}
 
-};
+];
 
 function appendToDisplayScreen(){
  calculation = number1 + number2;
@@ -28,13 +28,39 @@ function showResults(){
     let ageBox = document.getElementById("age").value;
     let emailBox = document.getElementById("email").value;
     let adressBox = document.getElementById("adress").value;
-    
+
      person.firstName = firstNameBox;
      person.lastName = lastNameBox;
      person.age = ageBox;
      person.email = emailBox;
      person.address = adressBox;
 
-    console.log(number1 +"\n" + person.firstName + "\n" + person.lastName)
     console.log(person)
+}
+
+function addingPeopleToTables(){
+    const newTable = document.createElement("table");
+    newTable.innerHTML = "<thead><th>First name</th><th>Last name</th><th>Age</th><th>Email</th><th>Adress</th></thead>";
+
+    for(personList of person){
+        const newRow = document.createElement("tr");
+        const tdFirstName = document.createElement("td");
+        const tdLastName = document.createElement("td");
+        const tdAge = document.createElement("td");
+        const tdEmail = document.createElement("td");
+        const tdAdress = document.createElement("td");
+        tdFirstName.textContent = person.firstName;
+        tdLastName.textContent = person.lastName;
+        tdAge.textContent = person.age;
+        tdEmail.textContent = person.email;
+        tdAdress.textContent = person.address;
+
+        newRow.appendChild(tdFirstName);
+        newRow.appendChild(tdLastName);
+        newRow.appendChild(tdAge);
+        newRow.appendChild(tdEmail);
+        newRow.appendChild(tdAdress);
+        newTable.appendChild(newRow);
+    }
+
 }
