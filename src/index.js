d33db04ad6a3e9f37b1a980f29455fa59e2d6373
Mikael <50752113+MikaelVV@users.noreply.github.com>
2024-07-14 13:15:50 +0300

    Testing various ways to collect input from user

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@
 let number1 = 1;
 let number2 = 2;
 let calculation;
+let firstName = prompt("Please enter your first name.");
+let lastName = prompt("Please enter your last name.");
+
 
 function appendToDisplayScreen(){
  calculation = number1 + number2;
- 
+
  console.log(calculation)
+}
+
+function showResults(){
+    
+    console.log(number1)
 }
